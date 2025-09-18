d037c5ef6e35f7ee320627142f51db943cf76e7d
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2025-09-18 17:54:11 +0530

    feat: initialize socket.io application with express server and basic chat functionality
    - Added package.json to manage dependencies including express, socket.io, and ejs.
    - Created server.js to set up an express server and integrate socket.io for real-time communication.
    - Implemented a basic chat feature where messages can be sent and received between the client and server.
    - Added index.ejs view to render a simple HTML page with a greeting and socket.io client-side script for chat interaction.

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+app.get("/",(req, res)=>{
+    res.render("index.ejs",{greet:"Good Morning"});
+})
+
+io.on("connection",(socket)=>{
+    console.log("a user connected", socket.id);
+    socket.on("chat",(msg)=>{
+        console.log("msg received->", msg);
+    });
+     socket.emit("chat", "thik hai badhiya");
+
+});
+server.listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
